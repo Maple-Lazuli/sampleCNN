ef5550fbedaf6fcdd04152820a34948833acf3a7
Ada <the.nostra.tymus@gmail.com>
2023-04-29 22:40:03 -0400

    updated data manager and data loader:

---
+++ data_utils/data_manager.py
@@ -0,0 +1,38 @@
+import json
+from dataclasses import dataclass
+
+import torch
+from ml_infrastructure.data_manager import DataManager
+
+from data_utils.dataset import Dataset
+
+@dataclass
+class DataManager:
+    batch_size: int = 5
+    training_noise: bool = False
+    stats: str = './data/stats.json'
+    train: str = './data/training.csv'
+    test: str = './data/test.csv'
+    val: str = './data/val.csv'
+
+    def __post_init__(self):
+        with open(self.stats, 'r') as file_in:
+            stats = json.load(file_in)
+
+        train_set = Dataset(csv=self.train, mu=stats['mu'], sigma=stats['sigma'],
+                            noise=self.training_noise)
+        self.train_loader = torch.utils.data.DataLoader(train_set, batch_size=self.batch_size,
+                                                        shuffle=True, num_workers=2)
+
+        test_set = Dataset(csv=self.test, mu=stats['mu'], sigma=stats['sigma'])
+        self.test_loader = torch.utils.data.DataLoader(test_set, batch_size=self.batch_size,
+                                                       shuffle=False, num_workers=2)
+
+        val_set = Dataset(csv=self.val, mu=stats['mu'], sigma=stats['sigma'])
+        self.val_loader = torch.utils.data.DataLoader(val_set, batch_size=self.batch_size,
+                                                      shuffle=False, num_workers=2)
+
+        self.classes = ([str(x) for x in range(1, 10)])
+
+        self.dm = DataManager(train_loader=self.train_loader, validation_loader=self.val_loader,
+                              test_loader=self.test_loader, classes=self.classes)
--- data_utils/dataset.py
+++ data_utils/dataset.py
@@ -2,3 +2,38 @@ from torch.utils.data import Dataset
 import pandas as pd
 import numpy as np
 import torch
+
+
+def add_noise(image):
+    row, col = image.shape
+    mean = 0
+    var = 0.1
+    sigma = var ** 0.5
+    gauss = np.random.normal(mean, sigma, (row, col))
+    gauss = gauss.reshape(row, col)
+    return image + gauss
+
+
+class Dataset(Dataset):
+
+    def __init__(self, csv, mu=0, sigma=1, noise=False):
+        self.annotations = pd.read_csv(csv)
+        self.mu = mu
+        self.sigma = sigma
+        self.noise = noise
+
+    def __len__(self):
+        return len(self.annotations)
+
+    def __getitem__(self, index):
+        example = self.annotations.iloc[index]
+        img = (np.fromfile(example['name'], dtype=np.uint8).reshape(28, 28) - self.mu) / self.sigma
+        if self.noise:
+            img = add_noise(img)
+        img = torch.from_numpy(img)
+        img = img.unsqueeze(0).repeat(1, 1, 1)
+        img = img.to(torch.float32)
+
+        target = torch.tensor(example['status']).unsqueeze(0).type(torch.float32)
+
+        return img, target
